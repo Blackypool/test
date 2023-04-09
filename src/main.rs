b6e5245f43e455255744896001ee57b54f49fd93
Andrew <syn.kolbasyn.06@gmail.com>
2023-04-09 21:40:36 +0300

    Added user interface

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@ use std::io::{stdin, stdout, Write};
 
 fn main(){
     let mut name: String = String::new();
+    stdout().write("Enter your name: ").unwrap();
+    stdout().flash().unwrap();
     stdin().read_line(&mut name).unwrap();
     name.pop();
     stdout().write(format!("Hello {name}!\n").as_bytes()).unwrap();
-    
 }
