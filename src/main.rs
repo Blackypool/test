159eae93efdb49643fcf07a8e15bc17f7163ca8d
Blacky <artem888bogdanov@mail.ru>
2023-04-09 21:43:38 +0300

    bug fixed

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ use std::io::{stdin, stdout, Write};
 
 fn main(){
     let mut name: String = String::new();
-    stdout().write("Enter your name: ").unwrap();
-    stdout().flash().unwrap();
+    stdout().write("Enter your name: ".as_bytes()).unwrap();
+    stdout().flush().unwrap();
     stdin().read_line(&mut name).unwrap();
     name.pop();
     stdout().write(format!("Hello {name}!\n").as_bytes()).unwrap();
